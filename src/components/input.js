16242bd50340a1ca73242421a661fca89ca4e148
Tommy Albinsson <tommy.albinsson@gmail.com>
2016-03-13 13:39:44 -0400

    Removed setting true of autoFocus.

--- src/components/input.js
+++ src/components/input.js
@@ -4,7 +4,7 @@ import './input.scss'
 export default function Input ({text, onChange}) {
   return (
     <div className='input'>
-      <input type='text' value={text} onChange={onChange} autoFocus={true} />
+      <input type='text' value={text} onChange={onChange} autoFocus />
     </div>
   )
 }
